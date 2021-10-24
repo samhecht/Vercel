859bdae324eb6702e2c9703aa1c332d0f1e0d0c1
EVOAUCTION <89047635+EVOAUCTION@users.noreply.github.com>
2021-10-24 13:23:46 -0400

    Create Footer.js

---
+++ components/Competition/Footer.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Container, Button, Col, Row, NavLink } from "reactstrap";
+
+import { Menu, Dropdown, Card } from "antd";
+
+const Footer = () => {
+  return (
+    <div style={{ marginTop: 0, backgroundColor: "black", height: 200 }}>
+      <h1 style={{ color: "white", textAlign: "center", margin: "auto", padding:80 }}>
+        <Button>Hello</Button>
+      </h1>
+    </div>
+  );
+};
+export default Footer;
