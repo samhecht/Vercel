6f5738db1dfe0450cc923ff455b1efbd8f49110d
EVOAUCTION <89047635+EVOAUCTION@users.noreply.github.com>
2021-10-24 13:22:42 -0400

    Create Ads.js

---
+++ components/Competition/Ads.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Container, Button, Col, Row, NavLink } from "reactstrap";
+
+import { Menu, Dropdown, Card } from "antd";
+
+
+const Ads = () => {
+return (
+
+    <div style={{ maxWidth: 1200, backgroundColor: "white", height: 200, margin:0 }}>
+      <h2 style={{ color: "black", paddingTop: 75,textAlign:'left' }}>
+        STYLES
+      </h2>
+    </div>
+
+);
+}
+    export default Ads;
